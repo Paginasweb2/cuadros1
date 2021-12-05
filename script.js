ef51d75220065bc083bcac60c94b9d38f0200202
Paginasweb2 <91202019+Paginasweb2@users.noreply.github.com>
2021-12-05 17:31:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function texto(){
+	var texto=document.getElementById("texto1").value;
+	document.getElementById("texto2").innerHTML="";
+	document.getElementById("titulo").innerHTML="<h1 class='titulo'>"+texto+"<h1>";
+}
+ 	
